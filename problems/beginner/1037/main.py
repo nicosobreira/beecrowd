a113f37cbbfd09beb7d3bbf2f1c25ac9bb71077e
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-03 14:58:23 -0300

    1037

---
+++ problems/beginner/1037/main.py
@@ -0,0 +1,27 @@
+"""
+A = float(input())
+
+if A >= 0 and A <= 25:
+    print("Intervalo [0,25]")
+elif A > 25 and A <= 50:
+    print("Intervalo (25,50]")
+elif A > 50 and A <= 75:
+    print("Intervalo (50,75]")
+elif A > 75 and A <= 100:
+    print("Intervalo (75,100]")
+else:
+    print("Fora de intervalo")
+"""
+
+A = float(input())
+
+if 0 <= A <= 25:
+    print("Intervalo [0,25]")
+elif 25 < A <= 50:
+    print("Intervalo (25,50]")
+elif 50 < A <= 75:
+    print("Intervalo (50,75]")
+elif 75 < A <= 100:
+    print("Intervalo (75,100]")
+else:
+    print("Fora de intervalo")
