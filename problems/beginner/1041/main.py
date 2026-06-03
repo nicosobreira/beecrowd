0ffae68a5ae04b1962238abbe751b7bd08afbb99
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-03 15:58:31 -0300

    1041

---
+++ problems/beginner/1041/main.py
@@ -0,0 +1,29 @@
+X, Y = map(float, input().split())
+
+if X == Y == 0:
+    print("Origem")
+elif X == 0:
+    print("Eixo Y")
+elif Y == 0:
+    print("Eixo X")
+elif X > 0:
+    print("Q1" if Y > 0 else "Q4")
+else:
+    print("Q2" if Y > 0 else "Q3")
+
+"""
+if X == Y == 0:
+    print("Origem")
+elif X == 0:
+    print("Eixo Y")
+elif Y == 0:
+    print("Eixo X")
+elif X > 0 and Y > 0:
+    print("Q1")
+elif X < 0 and Y > 0:
+    print("Q2")
+elif X < 0 and Y < 0:
+    print("Q3")
+elif X > 0 and Y < 0:
+    print("Q4")
+"""
